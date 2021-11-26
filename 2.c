200c92b3036da10fcc1656dc826ff89882797ff0
2pasha <90461506+2pasha@users.noreply.github.com>
2021-11-26 17:14:53 +0200

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,44 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <cs50.h>
-#include <string.h>
-
-int main ()
-{
-	int n;
-	n = get_int("Enter the number of characters: ");
-	char a[n];
-	printf("Enter characters: ");
-	
-	for (int i = 0; i < n; i++)
-	{
-		scanf ("%c", &a[i]);
-	}
-	
-	for (int j = 0; j < n / 2; j++)
-	{
-		char temp = a[j];
-		a[j] = a[n - j - 1];
-		a[n - j - 1] = temp;
-	}
-	printf("Inverted array: \n");
-	for (int i = 0; i < n; i++)
-	{
-		printf("%d ", a[i]);
-	}
-	return 0;
-}
-
-
-
-// 2-й варіант коду
-#include <stdio.h>
-#include <stdlib.h>
 #include <cs50.h>
 #include <string.h>
 int main ()
 {
-    string s = get_string("Enter: ");
+    string s = get_string("Enter array (without space): ");
     for(int i = strlen(s); i > 0; i--)
     {
         printf("%c", s[i]);
