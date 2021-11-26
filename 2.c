716ffa58cc9e72bebcf1673cfa2432c69bd9867e
2pasha <90461506+2pasha@users.noreply.github.com>
2021-11-26 18:35:40 +0200

    Update 2.c

--- 2.c
+++ 2.c
@@ -5,7 +5,7 @@ int main ()
     string s = get_string("Enter array (without space): ");
     for(int i = strlen(s); i > 0; i--)
     {
-        printf("%c", s[i]);
+        printf("Inverse array: %c", s[i]);
     }
     return 0;
 }
